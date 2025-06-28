629bd0ea5feb50ded4ba467f05feef3049fa8dbc
nurre24 <mijunnaa@gmail.com>
2025-06-28 14:06:18 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 70)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 70)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 70)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 0)
+    }
+})
